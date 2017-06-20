2fb28ffbc7c0632dc91250ce9ee26aac3ffd2959
Travis <travispinson@gmail.com>
2017-06-19 20:15:44 -0500

    add wikipedia results

--- js/script.js
+++ js/script.js
@@ -7,17 +7,13 @@ var markers = [];
 
 
 var ViewModel = function(map, markers) {
-  zooTweets = "<a class='twitter-timeline' href='https://www.twitter.com/hashtag/NashvilleZoo' data-widget-id='868900216082190336'>Tweets</a>";
-  scienceTweets = "<a class='twitter-timeline' href='https://twitter.com/search?q=adventure%20science%20center' data-widget-id='868889405167194112'>Tweets</a>";
-  parthenonTweets = "<a class='twitter-timeline href='https://twitter.com/search?q=%23nashville%20%23parthenon' data-widget-id='868899128100753408'>Tweets</a>";
-  bridgestoneTweets = "<a class='twitter-timeline' href='https://twitter.com/hashtag/BridgestoneArena' data-widget-id='868898437907042304'>Tweets</a>";
-  tpacTweets = "<a class='twitter-timeline' href='https://twitter.com/hashtag/TPAC' data-widget-id='869261143428259840'>Tweets</a>";
   locations = [
-  { title: 'Bridgestone Arena', location: {lat: 36.1592, lng: -86.7785}, id: 0, tweets: bridgestoneTweets},
-  { title: 'Nashville Zoo', location: {lat: 36.1392, lng: -86.7415}, id: 1, tweets: zooTweets },
-  { title: 'The Parthenon', location: {lat: 36.1497, lng: -86.8133}, id: 2, tweets: parthenonTweets},
-  { title: 'Adventure Science Center', location: {lat: 36.1465, lng: -86.7754}, id: 3, tweets: scienceTweets},
-  { title: 'Tennessee Performing Arts Center', location: {lat: 36.166156, lng: -86.776865}, id: 4, tweets: tpacTweets}];
+  { title: 'Bridgestone Arena', location: {lat: 36.1592, lng: -86.7785}, id: 0, tweets: "<a class='twitter-timeline' href='https://twitter.com/hashtag/BridgestoneArena' data-widget-id='868898437907042304'>Tweets</a>"},
+  { title: 'Nashville Zoo', location: {lat: 36.1392, lng: -86.7415}, id: 1, tweets: "<a class='twitter-timeline' href='https://www.twitter.com/hashtag/NashvilleZoo' data-widget-id='868900216082190336'>Tweets</a>"},
+  { title: 'Nashville Parthenon', location: {lat: 36.1497, lng: -86.8133}, id: 2, tweets: "<a class='twitter-timeline' href='https://twitter.com/search?q=%23nashville%20%23parthenon' data-widget-id='868899128100753408'>Tweets</a>"},
+  { title: 'Adventure Science Center', location: {lat: 36.1465, lng: -86.7754}, id: 3, tweets: "<a class='twitter-timeline' href='https://twitter.com/search?q=adventure%20science%20center' data-widget-id='868889405167194112'>Tweets</a>"},
+  { title: 'Tennessee Performing Arts Center', location: {lat: 36.166156, lng: -86.776865}, id: 4, tweets: "<a class='twitter-timeline' href='https://twitter.com/hashtag/TPAC' data-widget-id='869261143428259840'>Tweets</a>"}
+];
   this.locList = ko.observableArray([]);
   this.chosenHotspot = ko.observable();
   this.currentLoc = ko.observable( this.locList()[0] );
@@ -69,22 +65,24 @@ function showListings() {
 }
 
 
-  function showOneListing() {
-    var id = document.getElementById("idBox").innerHTML;
-    for (var i = 0; i < markers.length; i++) {
-      if  (id != locations[i].id) {
-        markers[id].setVisible(false);
-        bounds.extend(markers[id].position);
-        map.fitBounds(bounds);
-      }
+function showOneListing() {
+  var id = document.getElementById("idBox").innerHTML;
+  for (var i = 0; i < markers.length; i++) {
+    if  (id != locations[i].id) {
+      markers[id].setVisible(false);
+      bounds.extend(markers[id].position);
+      map.fitBounds(bounds);
     }
   }
+}
 
 
 // This function will loop through the listings and hide them all.
 function hideMarkers(markers) {
   for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);}}
+    markers[i].setMap(null);
+  }
+}
 
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -96,21 +94,10 @@ function populateInfoWindow(marker, infowindow) {
   if (infowindow.marker != marker) {
     infowindow.marker = "marker";
     infowindow.setContent(tweetsOpen);
-    if (marker.title == locations[1].title) {
-      infowindow.setContent(tweetsOpen + zooTweets);
+    for (var i = 0; i < markers.length; i++)
+    if (marker.title == locations[i].title) {
+      infowindow.setContent(tweetsOpen + locations[i].tweets);
       twttr.widgets.load(document.getElementById("infobox"))};
-    if (marker.title == "Adventure Science Center") {
-      infowindow.setContent(tweetsOpen + scienceTweets);
-      twttr.widgets.load(document.getElementById("infobox"))};
-    if (marker.title == "The Parthenon") {
-      infowindow.setContent(tweetsOpen + parthenonTweets);
-      twttr.widgets.load(document.getElementById("infobox"));}
-    if (marker.title == "Bridgestone Arena") {
-      infowindow.setContent(tweetsOpen + bridgestoneTweets);
-      twttr.widgets.load(document.getElementById("infobox"));}
-    if (marker.title == "Tennessee Performing Arts Center") {
-      infowindow.setContent(tweetsOpen + tpacTweets);
-      twttr.widgets.load(document.getElementById("infobox"));};
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick',function(){
@@ -124,3 +111,33 @@ ko.applyBindings(new ViewModel);
 
 // API IMPORT FROM TWITTER //
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+
+function loadData() {
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    // clear out old data before new request
+    $wikiElem.text("");
+    // load wikipedia data
+    var workingTitle = document.getElementById('titleBox').innerHTML;
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + document.getElementById('titleBox').innerHTML + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+    return false;
+};
